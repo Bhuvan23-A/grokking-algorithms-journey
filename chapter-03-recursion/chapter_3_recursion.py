cccbd271c310820a1a22aa5f44b72b30649f56a6
Bhuvan Sai Adithya Masarapu <bhuvanadithya23@gmail.com>
2025-12-27 12:26:51 +0530

    Add files via upload

---
+++ chapter-03-recursion/chapter_3_recursion.py
@@ -0,0 +1,5 @@
+#3.1. Greet is a base function and it calls greet2 function and both function use the variable name = maggie 
+# first greet2 function is will be done after that greet funtion 
+
+#3.2. stack overflow error will occur and memory will be used up bcoz u r going to call many function
+
