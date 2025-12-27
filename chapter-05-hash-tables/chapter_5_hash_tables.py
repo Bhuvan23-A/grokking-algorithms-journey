00d7cef7a843b1dbab1bd12dae2fd494b4a6fe67
Bhuvan Sai Adithya Masarapu <bhuvanadithya23@gmail.com>
2025-12-27 12:32:07 +0530

    Add files via upload

---
+++ chapter-05-hash-tables/chapter_5_hash_tables.py
@@ -0,0 +1,22 @@
+#5.1
+#This is consistent but it is not useful bcoz it always returns only 1 
+
+#5.2
+#This is not consistent bcoz it returns a random value for every input 
+
+#5.3
+#This is also not consistent bcoz it might return diff. values for same input.
+
+#5.4
+# This is consistent, bcoz it returns the same value for a string however many times you give it as an input.
+ 
+
+#5.5
+# For this phonebook, C and D would provide good distribution.
+ 
+#5.6
+# B and D would provide good distribution 
+
+#5.7
+# B and D would provide good distribution. As hash table has only 10 slots it will be difficult to provide a slot for each alphabet or else C will also if only 4 names are given like in the example.
+   
