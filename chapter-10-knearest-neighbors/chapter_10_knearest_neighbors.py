f3c31f66b67820cdf0242904f61dc3ddaaa118d7
Bhuvan Sai Adithya Masarapu <bhuvanadithya23@gmail.com>
2026-01-18 10:17:54 +0530

    Add files via upload

---
+++ chapter-10-knearest-neighbors/chapter_10_knearest_neighbors.py
@@ -0,0 +1,7 @@
+#10.1. Mayb any rating greater than 3 can be rated as a good and less than 3 can be rated as bad. Now as both yogi and pinky have similar taste they both will have similar good or bad movies. This is data simplification.This does not work
+# Data Normalization should be done. Look at the avg rating of each person and use it to scale their ratings. 
+
+#10.2. Influencer ratings can be given higher weights than normal user's ratings.
+
+#10.3. I think it is too low. bcoz they can be biased.
+# Acc to book if u have n users, you should look at sqrt(n) neighbors
